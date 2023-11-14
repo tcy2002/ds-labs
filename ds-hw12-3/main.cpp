c714c2566cf319e891c23ea3fad6433fea6a85bd
tcy2002 <1160826230@qq.com>
2023-11-14 22:54:32 +0800

    feat: new version of thread pool

--- ds-hw12-3/main.cpp
+++ ds-hw12-3/main.cpp
@@ -1,21 +1,23 @@
 #include <iostream>
 #include <vector>
+#include <random>
 #include <thread>
 #include <mutex>
+#include <atomic>
 #include <queue>
 #include <condition_variable>
 #include <functional>
 #include <windows.h>
 
-class ThreadPool {
+class MyThreadPool {
 private:
     using Task = std::function<void()>;
     uint32_t size;
-    std::vector<std::thread> pool;
+    std::vector<std::thread*> pool;
     std::queue<Task> tasks;
     std::mutex mtx;
     std::condition_variable cv;
-    bool stop;
+    std::atomic<bool> stop;
 
     void thread_wrapper() {
         while(true) {
@@ -24,7 +26,6 @@ private:
                 if (stop) return;
                 cv.wait(lock);
             }
-
             Task task(std::move(tasks.front()));
             tasks.pop();
             lock.unlock();
@@ -34,37 +35,42 @@ private:
     }
 
 public:
-    explicit ThreadPool(size_t pool_size = 0):
-        stop(false),
-        size(pool_size > 0 ? pool_size : std::thread::hardware_concurrency()) {
+    explicit MyThreadPool(size_t pool_size = 0):
+            stop(false),
+            size(pool_size > 0 ? pool_size : std::thread::hardware_concurrency()) {
         std::cout << "pool size: " << size << std::endl;
         for (int i = 0; i < size; i++) {
-            pool.emplace_back(&ThreadPool::thread_wrapper, this);
+            pool.push_back(new std::thread(&MyThreadPool::thread_wrapper, this));
         }
     }
-    ~ThreadPool() {
+    ~MyThreadPool() {
         join();
         stop = true;
         cv.notify_all();
-        for (auto& th : pool) {
-            if (th.joinable())
-                th.join();
+        for (auto th : pool) {
+            if (th->joinable())
+                th->join();
+            delete th;
         }
     }
 
     template<typename Function, typename... Args>
-    void addTask(Function&& fn, Args&&... args) {
-        std::unique_lock<std::mutex> lock(mtx);
+    void addTask(Function fn, Args&&... args) {
+        mtx.lock();
         tasks.emplace([fn, &args...]{ fn(args...); });
+        mtx.unlock();
         cv.notify_one();
     }
 
     template <typename Iterator, typename Function>
-    void forEach(Iterator first, Iterator last, Function&& fn) {
+    void forEach(Iterator first, Iterator last, Function fn) {
+        mtx.lock();
         while (first != last) {
-            addTask(fn, *first);
+            tasks.emplace([fn, &first]{ fn(*first); });
             ++first;
         }
+        mtx.unlock();
+        cv.notify_all();
     }
 
     void join() {
@@ -75,61 +81,50 @@ public:
     }
 };
 
-class Test {
-public:
-    int _val;
-    Test(): _val(0) { std::cout << "default constructor" << std::endl; }
-    explicit Test(int val): _val(val) { std::cout << "constructor" << std::endl; }
-    Test(const Test& t): _val(t._val) { std::cout << "copy constructor" << std::endl; }
-    Test(Test&& t) noexcept: _val(t._val) { std::cout << "move constructor" << std::endl; }
-};
+void exec(uint64_t& t) {
+    std::default_random_engine e(GetTickCount());
+    std::uniform_int_distribution<int> u(500, 1000);
+    auto mat = new uint64_t[100];
 
-void exec(Test& t) {
-    t._val++;
-    Sleep(10);
+    for (int i = 0; i < 100; i++) {
+        mat[i] = u(e) % 111;
+    }
+    for (int i = 0; i < u(e); i++) {
+        for (int j = 0; j < 100; j++) {
+            for(int k = 0; k < 100; k++) {
+                mat[j] = mat[j] * mat[k];
+            }
+        }
+    }
+
+    delete[] mat;
 }
 
 int main() {
-    LARGE_INTEGER freq_;
-    QueryPerformanceFrequency(&freq_);
-    LARGE_INTEGER begin_time;
-    LARGE_INTEGER end_time;
+    DWORD start, time1, time2;
 
-    // to initialize the vector and thread pool
-    ThreadPool pool;
-    std::vector<Test> vec;
-    vec.reserve(100);
-    for (int i = 0; i < 100; i++) {
-        vec.emplace_back(i);
-    }
-    std::cout << "init finished" << std::endl;
+    MyThreadPool pool(16);
+    int vec_size = 256;
+    auto vec = new uint64_t[vec_size]{};
+    std::cout << "start test" << std::endl;
 
     // sequential time
-    QueryPerformanceCounter(&begin_time);
-    for (auto& i : vec) {
-        exec(i);
+    start = GetTickCount();
+    for (int i = 0; i < vec_size; i++) {
+        exec(vec[i]);
     }
-    QueryPerformanceCounter(&end_time);
-    auto time1 = (double)(end_time.QuadPart - begin_time.QuadPart) * 1000.0 / (double)freq_.QuadPart;
+    time1 = GetTickCount() - start;
     std::cout << "sequential time: " << time1 << "ms" << std::endl;
 
     // parallel time
-    QueryPerformanceCounter(&begin_time);
-    pool.forEach(vec.begin(), vec.end(), exec);
+    start = GetTickCount();
+    pool.forEach(&vec[0], &vec[vec_size - 1], exec);
     pool.join();
-    QueryPerformanceCounter(&end_time);
-    auto time2 = (double)(end_time.QuadPart - begin_time.QuadPart) * 1000.0 / (double)freq_.QuadPart;
+    time2 = GetTickCount() - start;
     std::cout << "parallel time: " << time2 << "ms" << std::endl;
 
     // parallel ratio
-    std::cout << "parallel ratio: " << time1 / time2 << std::endl;
-
-    // to print and check the executed vector
-    int sum = 0;
-    for (auto& i : vec) {
-        sum += i._val;
-    }
-    std::cout << "sum: " << sum << std::endl;
+    std::cout << "parallel ratio: " << 1.0 * time1 / time2 << std::endl;
 
     return 0;
 }
